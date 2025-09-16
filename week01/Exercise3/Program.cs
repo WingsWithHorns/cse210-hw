f571cb7d6daa11363b0573936f2f6a98b78da592
WingsWithHorns <2wings.with.2horns@gmail.com>
2025-09-16 12:00:31 -0600

    excersize 3: loops is completed

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
+        Console.WriteLine("Guess the magic number: ");
+        int guessNumber = int.Parse(Console.ReadLine());
+        while (guessNumber != magicNumber)
+        {
+            if (guessNumber > magicNumber)
+            {
+                Console.WriteLine("Your guess was too high. try again. ");
+                guessNumber = int.Parse(Console.ReadLine());
+            }
+            else if (guessNumber < magicNumber)
+            {
+                Console.WriteLine("Your guess was too low. try again. ");
+                guessNumber = int.Parse(Console.ReadLine());
+            }
+            else
+            {
+                Console.WriteLine("That is an invalid guess. try again. ");
+                guessNumber = int.Parse(Console.ReadLine());
+            }
+        }
+        Console.WriteLine($"The magic number was {magicNumber}! You got it right!");
     }
 }

167504e24249dc3076331e1701893f08f386152f
WingsWithHorns <2wings.with.2horns@gmail.com>
2025-09-15 21:16:59 -0600

    Excersize2: grades is completed

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,53 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("What is your grade percentage?");
+        string gradeInput = Console.ReadLine();
+        double gradeValue = double.Parse(gradeInput);
+        string gradeLetter = "";
+        string pass = "";
+        if (gradeValue >= 90)
+        {
+            gradeLetter = "A";
+        }
+        else if (gradeValue >= 80)
+        {
+            gradeLetter = "B";
+        }
+        else if (gradeValue >= 70)
+        {
+            gradeLetter = "C";
+        }
+        else if (gradeValue >= 60)
+        {
+            gradeLetter = "D";
+        }
+        else if (gradeValue < 60)
+        {
+            gradeLetter = "F";
+        }
+        else
+        {
+            gradeLetter = "Invalid Score";
+        }
+        if (gradeValue >= 70)
+        {
+            pass = "Congrats! You passed the class!";
+        }
+        else if (gradeValue < 70)
+        {
+            pass = "Sorry, you did not pass the class, try again.";
+        }
+        else
+        {
+            pass = "Invalid Score";
+        }
+        Console.WriteLine($"Your grade is a {gradeLetter}.");
+        Console.WriteLine($"{pass}");
+
     }
 }

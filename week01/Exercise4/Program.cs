9cf7fb75fe10a1defe0bc585bd62200eb7e2cffc
WingsWithHorns <2wings.with.2horns@gmail.com>
2025-09-16 13:45:46 -0600

    exercise4 working with lists has been added.

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished. ");
+        int input = 1;
+        List<int> numbers = new List<int>();
+        while (input != 0)
+        {
+            Console.WriteLine("Enter number: ");
+            input = int.Parse(Console.ReadLine());
+            if (input != 0)
+            {
+                numbers.Add(input);
+            }
+        }
+        int total = 0;
+        foreach (int i in numbers)
+        {
+            total += i;
+        }
+        Console.WriteLine($"The total of your numbers is {total}.");
+        float average = ((float)total) / numbers.Count;
+        Console.WriteLine($"The average of your numbers is {average}.");
+        int max = numbers[0];
+        foreach (int i in numbers)
+        {
+            if (i > max)
+            {
+                max = i;
+            }
+        }
+        Console.WriteLine($"The max number in your set is {max}.");
+
+
     }
 }
